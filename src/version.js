d6fd64a39a060448c82d5fb05dcb453ba08d79e3
Nuno Campos <nuno@crowdprocess.com>
2016-06-04 13:45:57 +0100

    karma tests on all versions of react - rewrote src/version.js in ES5 to be able to use it in karma.conf.js - conditionally add webpack.IgnorePlugin based on React version installed, mirrorring react-compat.js

--- src/version.js
+++ src/version.js
@@ -1,5 +1,12 @@
-import React from 'react';
-export const VERSION = React.version;
-export const REACT013 = VERSION.slice(0, 4) === '0.13';
-export const REACT014 = VERSION.slice(0, 4) === '0.14';
-export const REACT15 = VERSION.slice(0, 3) === '15.';
+/* eslint-disable no-var,object-shorthand */
+
+var React = require('react');
+
+var VERSION = React.version;
+
+module.exports = {
+  VERSION: VERSION,
+  REACT013: VERSION.slice(0, 4) === '0.13',
+  REACT014: VERSION.slice(0, 4) === '0.14',
+  REACT15: VERSION.slice(0, 3) === '15.',
+};
